a92581158a3490d85a078fd41a753d420b2731a9
gallindo <gallindo@gmail.com>
2016-09-02 18:43:59 -0300

    WIP

--- commit-counter.sh
+++ commit-counter.sh
@@ -13,11 +13,11 @@ git pull -r origin develop
 
 GIT_SHORT_LOG=$(git shortlog --summary --numbered --email b8bc8fca15e0303681b9b59772ff487d933a5d1b..HEAD)
 
-GIT_LIST_PULL_REQUESTS=$(java -jar /Users/miguelangelo/Development/workspace/github-personal-statistics/build/libs/github-personal-statistics.jar)
+#GIT_LIST_PULL_REQUESTS=$(java -jar /Users/miguelangelo/Development/workspace/github-personal-statistics/build/libs/github-personal-statistics.jar)
 
 echo "$GIT_SHORT_LOG"
 
-echo "$GIT_LIST_PULL_REQUESTS"
+#echo "$GIT_LIST_PULL_REQUESTS"
 
 osascript -e "display notification \"${GIT_SHORT_LOG}\" with title \"Summary\""
 
--- git-daily.sh
+++ git-daily.sh
@@ -1,13 +1,40 @@
-#!/usr/local/bin/bash
+#!/bin/sh -e
 # OUTPUT-COLORING
 
+#Black        0;30     Dark Gray     1;30
+#Red          0;31     Light Red     1;31
+#Green        0;32     Light Green   1;32
+#Brown/Orange 0;33     Yellow        1;33
+#Blue         0;34     Light Blue    1;34
+#Purple       0;35     Light Purple  1;35
+#Cyan         0;36     Light Cyan    1;36
+#Light Gray   0;37     White         1;37
+
 set -o errexit ; set -o nounset
+
+# Colors
+reset_color="\033[0m"            # Reset color
+summary_color="\033[0;32m"
+issue_color="\033[0;33m"          # Yellow
+date_color="\033[0;35"
 
 cd /Users/miguelangelo/Liferay/GS/git/bg/upstream-bgp
 
 git pull -r origin develop
 
-GIT_DAILY_LOG=$(git log --oneline --after="3 days ago" --reverse --author="gallindo" --date=iso-strict-local --pretty=format:"%ad %s (%an)")
+function lastworkingday()
+{
+    if [[ "1" == "$(date +%u)" ]]; then
+        echo "last friday"
+    else
+        echo "yesterday"
+    fi
+}
+
+AUTHOR=${AUTHOR:="$(git config --global user.name)"}
+SINCE=${SINCE:="$(lastworkingday)"}
+
+GIT_DAILY_LOG=$(git log --oneline --after="$SINCE" --reverse --author="$AUTHOR" --date=iso-strict-local --pretty=format:"%ad %s (%an)")
 
 #declare -A JIRA_ISSUES
 
@@ -20,7 +47,7 @@ do
 
     JIRA_ISSUE_DESCRIPTION=$(jira show -o summary $JIRA_ISSUE_ID)
 
-    echo "$COMMIT_DATE | [$JIRA_ISSUE_ID - $JIRA_ISSUE_DESCRIPTION] | $COMMIT_MESSAGE"
+    echo "$COMMIT_DATE \n    [${issue_color}$JIRA_ISSUE_ID${reset_color} - ${summary_color}$JIRA_ISSUE_DESCRIPTION${reset_color}] \n        $COMMIT_MESSAGE"
 done <<< "$GIT_DAILY_LOG"
 
 cd -
